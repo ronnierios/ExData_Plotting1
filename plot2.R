163d1eacee0516a7a2099cdc051fb6b83c14b532
Ronnie Rios <ronnierios@outlook.com>
2014-10-12 13:10:04 -0600

    second plot ended

---
+++ plot2.R
@@ -0,0 +1,20 @@
+plot2 <- function()
+{
+  temp <- "household_power_consumption.zip"
+  
+  if (file.exists(temp) == FALSE)
+  {
+    message("Zip file no found in working directory. Downloading...")
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)     
+  }
+  data <- read.csv(file= unz(temp,"household_power_consumption.txt"), header=TRUE, sep=";",dec=".",na.strings="?")
+  #unlink(temp)  
+  data$Date <- as.Date(data$Date,"%d/%m/%Y")
+  data <- subset(data, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02") )
+  datetime <- as.POSIXct(paste(data$Date,data$Time))
+  plot(datetime,data$Global_active_power,type="l",ann=FALSE)
+  title(ylab="Global Active Power (kilowatts)")
+  #hist(data$Global_active_power,col="red",main=maintitle,xlab=paste(maintitle,"(kilowatts)"))
+  dev.copy(png,"plot2.png",width=480,height=480,units="px")
+  dev.off()
+}
