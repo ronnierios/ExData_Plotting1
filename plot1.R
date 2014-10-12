68b5bf447d459d59723ef218651532d556eb9295
Ronnie Rios <ronnierios@outlook.com>
2014-10-12 14:11:27 -0600

    improved image resolution

--- plot1.R
+++ plot1.R
@@ -7,12 +7,11 @@ plot1 <- function()
     message("Zip file no found in working directory. Downloading...")
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)     
   }
-  data <- read.csv(file= unz(temp,"household_power_consumption.txt"), header=TRUE, sep=";",dec=".",na.strings="?")
-  #unlink(temp)  
+  data <- read.csv(file= unz(temp,"household_power_consumption.txt"), header=TRUE, sep=";",dec=".",na.strings="?")  
   data$Date <- as.Date(data$Date,"%d/%m/%Y")
   data <- subset(data, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02") )
   maintitle <- "Global Active Power"
+  png("plot1.png", width = 480, height = 480,units="px")
   hist(data$Global_active_power,col="red",main=maintitle,xlab=paste(maintitle,"(kilowatts)"))
-  dev.copy(png,"plot1.png",width=480,height=480,units="px")
   dev.off()
 }
